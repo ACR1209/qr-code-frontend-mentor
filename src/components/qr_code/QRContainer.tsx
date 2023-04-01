a6dd0181ddde222334d59bbb43c0f6a404089712
ACR1209 <andrescoronel1209@gmail.com>
2023-04-01 17:18:12 -0500

    Solved image

--- src/components/qr_code/QRContainer.tsx
+++ src/components/qr_code/QRContainer.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './QRContainer.module.css'
-
+import qr from '../../assets/image-qr-code.png'
 
 function QRContainer() {
   return (
     <div className={styles.container}>
-        <img className={styles.qr} src='../public/image-qr-code.png'></img>
+        <img className={styles.qr} src={qr}></img>
         <h1 className={styles.title}>Improve your front-end skills by building projects</h1>
         <p className={styles.description}>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
